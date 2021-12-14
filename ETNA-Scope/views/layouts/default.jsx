a711bcdf703b9586c9fc08adf664f6c692e87291
racha-hamia <rasha.hamia@icloud.com>
2021-12-14 14:46:17 +0100

    adding some rasha style

--- ETNA-Scope/views/layouts/default.jsx
+++ ETNA-Scope/views/layouts/default.jsx
@@ -1,12 +1,22 @@
 var React = require('react');
- 
+
+
 function DefaultLayout(props) {
   return (
     <html>
-      <head><title>{props.title}</title><link rel="stylesheet" href='/stylesheets/style.css'></link></head>
-      <body>{props.children}</body>
+      <head>
+        <title>{props.title}</title>
+        <link rel="stylesheet" href='/stylesheets/style.css'></link>
+        
+      </head>
+      <body>
+          
+          {props.children}
+          <script src="/js/index.js"></script>
+        
+      </body>
     </html>
   );
 }
- 
+
 module.exports = DefaultLayout;
--- ETNA-Scope/views/login/login.jsx
+++ ETNA-Scope/views/login/login.jsx
@@ -4,15 +4,18 @@ var React = require('react');
 function login(props) {
   return (
 
-
+<div className="app">
     <form method="POST" action="/">
-        <input type="text" name="login" placeholder="type your login" required></input>
-        <input type="password" name="password" placeholder="type your password" required></input>
-        <button type="submit">Submit</button>
+        <label for="login">Login :</label>
+        <input type="text" className="input" name="login" placeholder="type your login" required></input> <br /> <br /> 
+        <label for="password">Password :</label>
+        <input type="password" className="input" name="password" placeholder="type your password" required></input> 
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
 
+</div>
 
   );
 }
- 
+
 module.exports = login;
