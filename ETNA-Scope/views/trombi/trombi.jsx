25c817e5086d154bd0b8d6f305c0b97d63dfeb0d
giraul_a <giraul_a@etna-alternance.net>
2021-12-15 14:52:14 +0100

    debug image

--- ETNA-Scope/views/trombi/trombi.jsx
+++ ETNA-Scope/views/trombi/trombi.jsx
@@ -9,7 +9,7 @@ function Trombi(props) {
           <h3>{item.lastName} - {item.id}</h3>
           <p>{item.firstName}</p>
           
-          <img src={"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.20minutes.fr%2Fhigh-tech%2F2012251-20170210-troll-sommeille-chacun&psig=AOvVaw1lbvbxO81OJAybCDdZkTSp&ust=1639657821885000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKiNo8Ln5fQCFQAAAAAdAAAAABAD"} alt='new' height={150} width={200}/>
+          <img src={"https://auth.etna-alternance.net/api/users/habert_j/photo"} alt='new' height={150} width={175}/>
           <p>{item.mail}</p>
           <p>{item.phone}</p>
           <a href={"/"+item.id}>test </a>
