78ca55dfb873cbffbca30a7df20a1e17daed21d4
racha-hamia <rasha.hamia@icloud.com>
2021-12-16 16:11:25 +0100

    design du site psq vous etes rien sans moi

--- ETNA-Scope/views/trombi/trombi.jsx
+++ ETNA-Scope/views/trombi/trombi.jsx
@@ -6,13 +6,19 @@ function Trombi(props) {
   return (<div class="card">
     {props.data.map(((item) => (
       <div key={item.id} className="post">
-        <h3>{item.lastName} - {item.id}</h3>
-        <p>{item.firstName}</p>
-        <img src={item.pictureUrl} alt='new' height={150} width={175} />
-        <p>{item.mail}</p>
-        <p>{item.phone}</p>
-        <a href={"/" + item.id}>test </a>
-        <a href={"index/dl/" + item.id}>delete</a>
+          <div className="show">
+            <h3>{item.lastName} - {item.id}</h3>
+            <p>{item.firstName}</p>
+            <div className="img">
+              <img src={item.pictureUrl} alt='new' height={150} width={175} />
+            </div>
+          </div>
+          <div className="hide">
+            <p>{item.mail}</p>
+            <p>{item.phone}</p>
+            <a href={"/" + item.id}>test </a>
+            <a href={"index/dl/" + item.id}>delete</a>
+          </div>
       </div>
     )))}
 
